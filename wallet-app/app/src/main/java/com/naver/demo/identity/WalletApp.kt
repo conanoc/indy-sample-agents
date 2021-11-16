bbf6388acb76134761226fd6313dd9df3726567c
conanoc <conanoc@gmail.com>
2021-11-16 16:10:01 +0900

    Make the wallet open faster

--- wallet-app/app/src/main/java/com/naver/demo/identity/WalletApp.kt
+++ wallet-app/app/src/main/java/com/naver/demo/identity/WalletApp.kt
@@ -1,7 +1,6 @@
 package com.naver.demo.identity
 
 import android.app.Application
-import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.preference.PreferenceManager
 import android.system.Os
@@ -9,15 +8,9 @@ import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import org.hyperledger.indy.sdk.LibIndy
 import org.hyperledger.indy.sdk.anoncreds.Anoncreds
-import org.hyperledger.indy.sdk.pool.Pool
-import org.hyperledger.indy.sdk.pool.PoolJSONParameters.CreatePoolLedgerConfigJSONParameter
-import org.hyperledger.indy.sdk.pool.PoolLedgerConfigExistsException
 import org.hyperledger.indy.sdk.wallet.Wallet
-import org.hyperledger.indy.sdk.wallet.WalletExistsException
 import org.json.JSONObject
-import java.io.File
 import java.util.concurrent.ExecutionException
 
 
@@ -28,17 +21,31 @@ class WalletApp : Application() {
 
     private fun openWallet() {
         val walletConfig = JSONObject().put("id", "demoWallet").toString()
-        val walletCredentials = JSONObject().put("key", "1234").toString()
-        try {
-            Wallet.createWallet(walletConfig, walletCredentials).get()
-        } catch (e : ExecutionException) {
-            if (e.cause !is WalletExistsException)
+        val walletCredentials : String?
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val walletExists = pref.getBoolean("walletExists", false)
+
+        if (!walletExists) {
+            try {
+                val key = Wallet.generateWalletKey("{}").get()
+                walletCredentials = JSONObject().put("key", key).put("key_derivation_method", "RAW").toString()
+                Wallet.createWallet(walletConfig, walletCredentials).get()
+                with (pref.edit()) {
+                    putString("walletKey", key)
+                    putBoolean("walletExists", true)
+                    apply()
+                }
+            } catch (e: ExecutionException) {
                 throw e.cause!!
+            }
+        } else {
+            val key = pref.getString("walletKey", null)
+            walletCredentials = JSONObject().put("key", key).put("key_derivation_method", "RAW").toString()
         }
 
         indyWallet = Wallet.openWallet(walletConfig, walletCredentials).get()
         walletOpened = true
-        val pref = PreferenceManager.getDefaultSharedPreferences(this);
         masterSecretId = pref.getString("masterSecret", null)
         if (masterSecretId == null) {
             masterSecretId = Anoncreds.proverCreateMasterSecret(indyWallet, null).get()
@@ -54,7 +61,7 @@ class WalletApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        Os.setenv("EXTERNAL_STORAGE", applicationContext.filesDir.toString(), true);
+        Os.setenv("EXTERNAL_STORAGE", applicationContext.filesDir.toString(), true)
 
         GlobalScope.launch(Dispatchers.IO) {
             openWallet()
